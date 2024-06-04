3a40ce3a4bc3e5479c099b7a92d2d79f5ba6e5d7
Munkh-Batchuluun <munkh.bc@gmail.com>
2024-06-04 21:25:12 +1000

    added user and tested delete, put methods

--- 04. User Back-End/controllers/notes.js
+++ 04. User Back-End/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/node')
+const User = require('../models/user')
 
 notesRouter.get('/', (req, res) => {
     Note.find({}).then(note => {
@@ -15,6 +16,46 @@ notesRouter.get('/:id', (req, res, next) => {
             } else {
                 res.status(404).end()
             }
+        })
+        .catch(err => next(err))
+})
+
+notesRouter.post('/', async(req, res) => {
+    const body = req.body
+
+    const user = await User.findById(body.userId)
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        user: user.id
+    })
+
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    res.status(201).json(savedNote)
+})
+
+notesRouter.delete('/', async(req, res, next) => {
+    Note.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(err => next(err))
+})
+
+notesRouter.put('/:id', async(req, res, next) => {
+    const body = req.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(req.params.id, note, { new: true })
+        .then((updatedNote) => {
+            res.json(updatedNote)
         })
         .catch(err => next(err))
 })
--- 04. User Back-End/utils/middleware.js
+++ 04. User Back-End/utils/middleware.js
@@ -1,6 +1,6 @@
 const logger = require('./logger')
 
-const requestLogger = (req, next) => {
+const requestLogger = (req, res, next) => {
     logger.info('Method: ', req.method)
     logger.info('Path: ', req.path)
     logger.info('Body: ', req.body)
@@ -8,17 +8,19 @@ const requestLogger = (req, next) => {
     next()
 }
 
-const unknownEndpoint = (res) => {
+const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'Unknown end-point' })
 }
 
-const errorHandler = (err, res, next) => {
+const errorHandler = (err, req, res, next) => {
     logger.error(err)
 
-    if(err === 'CastError'){
-        return res.status(404).send({ error: 'Malformatted id'})
-    } else if(err === 'ValidationError'){
-        return res.status(404).json({ error: err.message})
+    if(err.name === 'CastError'){
+        return res.status(404).send({ error: 'Malformatted id' })
+    } else if(err.name === 'ValidationError'){
+        return res.status(404).json({ error: err.message })
+    } else if(err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error')){
+        return res.status(400).json({ error: 'Expected `username` to be unique' })
     }
 
     next(err)
